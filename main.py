ec2962c7adc7e5c866109bdeae0c042f6c6cfd34
HanaGabay <chanag053@gmail.com>
2024-12-18 20:07:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+# Import libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import fetch_openml
 
-reference_feature = selected_features[2]  # The reference feature
-comparison_feature = selected_features[4]  # A feature to compare to
+data = fetch_openml(name='diabetes', version=1, as_frame=True)
+print(data.DESCR)
+df = data.frame
+df.sample(5)
+
+features = df.columns
+selected_features = [features[1], features[2], features[4], features[6], features[7]]
+
+features = list(df.columns)
+print("Available features:", features)
+selected_features = [features[1], features[2], features[4], features[6], features[7]]
+print("Selected features: ", selected_features)
+
+reference_feature = selected_features[0]  # The reference feature
+comparison_feature = selected_features[2]  # A feature to compare to
 
 # Create a scatter plot for the selected pair
 plt.figure(figsize=(8, 6))
@@ -12,4 +29,3 @@ plt.ylabel(comparison_feature)
 plt.savefig('correlation_plot.png')
 
 plt.show()
-
